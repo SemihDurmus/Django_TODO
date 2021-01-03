19ab42739cb26cdf2e54b07f463bc16c5012c962
SemihDurmus <semihdurmus@yahoo.com>
2021-01-03 19:17:02 +0100

    Add: Initial files

--- src/todo_project/settings.py
+++ src/todo_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'rm2)8tfstd9fp31h8-ix^e$ud@1@&cdt40pn6lsj&hnr1-^bie'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'todo',
+    # 'todo.apps.TodoConfig'
 ]
 
 MIDDLEWARE = [
